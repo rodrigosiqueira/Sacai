d061e62f0e7c341f707319f9d9b87ad554b6a8e0
Rodrigo <rodrigosiqueiramelo@gmail.com>
2015-01-16 18:17:56 -0200

    Updated folder and file organization.

--- Readme.txt
+++
@@ -1,4 +0,0 @@
-Javari: competição cerimonial desportiva religiosa.
-ça-cai (Sacai): olhos inquietos. Nome comum de várias espécies de macacos
-da família dos Calitriquídeos , de pequeno porte, com os membros anteriores
-mais curtos que os posteriores, de cauda não preênsil, que ocorrem na América do Sul. 
---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+/**
+ * @file main.cpp
+ * @brief
+ * @see http://docs.opencv.org/doc/tutorials/calib3d/camera_calibration/camera_calibration.html
+ **/
+
+#include <iostream>
+#include <sstream>
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+#include <SacaiModel.hpp>
+#include <SacaiController.hpp>
+
+using namespace cv;
+using namespace std;
+
+//int argc, char* argv[]
+int main()
+{
+  SacaiModel * _model = new SacaiModel();
+  SacaiController * _control = new SacaiController(_model);
+  _control->showWindow(true);
+  return 0;
+}
